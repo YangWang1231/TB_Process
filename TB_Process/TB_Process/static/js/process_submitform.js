173ee847a43006dc745f0be52fc1ccfed8bdca36
WangYang1231 <pene_wang@126.com>
2019-08-17 22:50:03 +0800

    使用更规范的方式配置ajaxform

--- TB_Process/TB_Process/static/js/process_submitform.js
+++ TB_Process/TB_Process/static/js/process_submitform.js
@@ -3,14 +3,14 @@
     $(document).ready(function () {
         // bind 'myForm' and provide a simple callback function
         //alert("Thank you for your comment!");
-        $('#form1').ajaxForm(function () {
+        $('#formx').ajaxForm(function () {
             alert("Thank you for your comment!");
         });
     });
 
         $(function () {
         /** 验证文件是否导入成功  */
-        $("#form1").ajaxForm(function (data) {
+        $("#formx").ajaxForm(function (data) {
             if (data == "1") {
                 alert("提交成功！");
                 $('#submitover').text("提交成功，分析完毕会自动启动下载")
@@ -20,52 +20,56 @@
 
         $(document).ready(function() {
             var options = {
-        target: '#output2',   // target element(s) to be updated with server response
-    beforeSubmit:  showRequest,  // pre-submit callback
-    success:       showResponse  // post-submit callback
+           // target: '#output2',   // target element(s) to be updated with server response
+            beforeSubmit:   showRequest,  // pre-submit callback
+            success:            showResponse,  // post-submit callback
 
-    // other available options:
-    //url:       url         // override for form's 'action' attribute
-    //type:      type        // 'get' or 'post', override for form's 'method' attribute
-    //dataType:  null        // 'xml', 'script', or 'json' (expected server response type)
-    //clearForm: true        // clear all form fields after successful submit
-    //resetForm: true        // reset the form after successful submit
+            // other available options:
+            //url:       url         // override for form's 'action' attribute
+            //type:      type        // 'get' or 'post', override for form's 'method' attribute
+            //dataType:  null        // 'xml', 'script', or 'json' (expected server response type)
+            clearForm: true        // clear all form fields after successful submit
+            //resetForm: true        // reset the form after successful submit
 
-    // $.ajax options can be used here too, for example:
-    //timeout:   3000
-};
+            // $.ajax options can be used here too, for example:
+            //timeout:   3000
+        };
 
-// bind to the form's submit event
-            $('#myForm2').submit(function() {
-        // inside event callbacks 'this' is the DOM element so we first
-        // wrap it in a jQuery object and then invoke ajaxSubmit
-        $(this).ajaxSubmit(options);
+        // bind to the form's submit event
+            $('#form1').submit(function() {
+                // inside event callbacks 'this' is the DOM element so we first
+                // wrap it in a jQuery object and then invoke ajaxSubmit
+                $(this).ajaxSubmit(options);
 
-    // !!! Important !!!
-    // always return false to prevent standard browser submit and page navigation
-    return false;
-});
-});
+                // !!! Important !!!
+                // always return false to prevent standard browser submit and page navigation
+                return false;
+            });
+        });
 
 // pre-submit callback
-        function showRequest(formData, jqForm, options) {
-            // formData is an array; here we use $.param to convert it to a string to display it
-            // but the form plugin does this for you automatically when it submits the data
-            var queryString = $.param(formData);
+    function showRequest(formData, jqForm, options) {
+        // formData is an array; here we use $.param to convert it to a string to display it
+        // but the form plugin does this for you automatically when it submits the data
+        var queryString = $.param(formData);
 
-    // jqForm is a jQuery object encapsulating the form element.  To access the
-    // DOM element for the form do this:
-    // var formElement = jqForm[0];
+        // jqForm is a jQuery object encapsulating the form element.  To access the
+        // DOM element for the form do this:
+        // var formElement = jqForm[0];
 
-    alert('About to submit: \n\n' + queryString);
+        alert('About to submit: \n\n' + queryString);
 
-    // here we could return false to prevent the form from being submitted;
-    // returning anything other than false will allow the form submit to continue
-    return true;
+        // here we could return false to prevent the form from being submitted;
+        // returning anything other than false will allow the form submit to continue
+        return true;
 }
 
 // post-submit callback
         function showResponse(responseText, statusText)  {
+            if (responseText == "1") {
+                alert("提交成功！");
+                $('#submitover').text("提交成功，分析完毕会自动启动下载")
+            };
         // for normal html responses, the first argument to the success callback
         // is the XMLHttpRequest object's responseText property
 
@@ -77,6 +81,6 @@
         // property set to 'json' then the first argument to the success callback
         // is the json data object returned by the server
 
-        alert('status: ' + statusText + '\n\nresponseText: \n' + responseText +
-            '\n\nThe output div should have already been updated with the responseText.');
+        //alert('status: ' + statusText + '\n\nresponseText: \n' + responseText +
+        //    '\n\nThe output div should have already been updated with the responseText.');
     }
